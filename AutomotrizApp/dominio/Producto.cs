8cc5dedca51e961713576fec84d010478cdb0919
Federico Ahumada <ahufede@gmail.com>
2022-10-15 20:47:09 -0300

    first commit

---
+++ AutomotrizApp/dominio/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotrizApp.dominio
+{
+    internal class Producto
+    {
+        public int Id_Producto { get; set; }
+        public string Descripcion { get; set; }
+        public int Anio { get; set; }
+
+        //public bool Stock { get; set; }
+        public double Precio_Vta { get; set; }
+        public int Id_marca { get; set; }
+        public int Id_Tipo_Producto { get; set; }
+
+    }
+}
