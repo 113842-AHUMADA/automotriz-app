a68fb8fd77f9c002041f671f2b44a4c01ff5924c
Federico Ahumada <ahufede@gmail.com>
2022-10-23 02:50:50 -0300

    endpoints web api

--- AutomotrizApp/AutomotrizWebAPI/Controllers/PedidoController.cs
+++ AutomotrizApp/AutomotrizWebAPI/Controllers/PedidoController.cs
@@ -1,6 +1,7 @@
 ﻿using AutomotrizApp.dominio;
 using AutomotrizBack.negocio.implementaciones;
 using AutomotrizBack.negocio.interfaces;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -31,21 +32,50 @@ namespace AutomotrizWebAPI.Controllers
 
         }
 
-        [HttpPost("pedidos")]
+        [HttpGet("consultar")]
+        public IActionResult GetPedidos(DateTime desde, DateTime hasta, string cliente)
+        {
+            try
+            {
+                return Ok(app.ObtenerPedidosPorFiltro(desde,hasta,cliente));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno. Intente nuevamente");
+            }
+
+        }
+
+        [HttpPost("crear")]
         public IActionResult PostPedido(Pedido oPedido)
         {
             try
             {
                 if (oPedido == null)
-                    return BadRequest();
+                    return BadRequest("Objeto Pedido requerido");
                 else
                     return Ok(app.CrearPedido(oPedido));
             }
             catch (Exception ex)
             {
                 return StatusCode(500, "No se pudo registrar correctamente.");
-            };
+            }
+        }
 
+        [HttpDelete("eliminar")]
+        public IActionResult DeletePedido(int id)
+        {
+            try
+            {
+                if (id == 0)
+                    return BadRequest("Id es requerido");
+                else
+                    return Ok(app.BorrarPedido(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "No se pudo eliminar correctamente.");
+            }
         }
     }
 }
