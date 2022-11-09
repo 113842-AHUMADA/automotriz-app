55f6dfa3e7f59596be77bbe00870b7244306a9da
Luiten Lucas <lluiten83@gmail.com>
2022-11-09 18:44:39 -0300

    Automotriz: alta de vehículos

--- AutomotrizApp/AutomotrizFront/frmVehiculo.Designer.cs
+++ AutomotrizApp/AutomotrizFront/frmVehiculo.Designer.cs
@@ -32,12 +32,23 @@
             this.txtMarca = new System.Windows.Forms.TextBox();
             this.lblModelo = new System.Windows.Forms.Label();
             this.txtModelo = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.cboDescripcion = new System.Windows.Forms.ComboBox();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.lblAnio = new System.Windows.Forms.Label();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblMarca
             // 
             this.lblMarca.AutoSize = true;
-            this.lblMarca.Location = new System.Drawing.Point(36, 69);
+            this.lblMarca.Location = new System.Drawing.Point(269, 64);
             this.lblMarca.Name = "lblMarca";
             this.lblMarca.Size = new System.Drawing.Size(40, 15);
             this.lblMarca.TabIndex = 0;
@@ -45,7 +56,7 @@
             // 
             // txtMarca
             // 
-            this.txtMarca.Location = new System.Drawing.Point(121, 61);
+            this.txtMarca.Location = new System.Drawing.Point(354, 56);
             this.txtMarca.Name = "txtMarca";
             this.txtMarca.Size = new System.Drawing.Size(100, 23);
             this.txtMarca.TabIndex = 1;
@@ -53,7 +64,7 @@
             // lblModelo
             // 
             this.lblModelo.AutoSize = true;
-            this.lblModelo.Location = new System.Drawing.Point(36, 106);
+            this.lblModelo.Location = new System.Drawing.Point(269, 101);
             this.lblModelo.Name = "lblModelo";
             this.lblModelo.Size = new System.Drawing.Size(48, 15);
             this.lblModelo.TabIndex = 2;
@@ -61,16 +72,118 @@
             // 
             // txtModelo
             // 
-            this.txtModelo.Location = new System.Drawing.Point(121, 103);
+            this.txtModelo.Location = new System.Drawing.Point(354, 98);
             this.txtModelo.Name = "txtModelo";
             this.txtModelo.Size = new System.Drawing.Size(100, 23);
             this.txtModelo.TabIndex = 3;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(271, 140);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(30, 15);
+            this.lblDescripcion.TabIndex = 4;
+            this.lblDescripcion.Text = "Tipo";
+            // 
+            // cboDescripcion
+            // 
+            this.cboDescripcion.FormattingEnabled = true;
+            this.cboDescripcion.Location = new System.Drawing.Point(354, 140);
+            this.cboDescripcion.Name = "cboDescripcion";
+            this.cboDescripcion.Size = new System.Drawing.Size(121, 23);
+            this.cboDescripcion.TabIndex = 5;
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(271, 178);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(36, 15);
+            this.lblColor.TabIndex = 6;
+            this.lblColor.Text = "Color";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(354, 178);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 23);
+            this.txtColor.TabIndex = 7;
+            // 
+            // lblAnio
+            // 
+            this.lblAnio.AutoSize = true;
+            this.lblAnio.Location = new System.Drawing.Point(273, 216);
+            this.lblAnio.Name = "lblAnio";
+            this.lblAnio.Size = new System.Drawing.Size(29, 15);
+            this.lblAnio.TabIndex = 8;
+            this.lblAnio.Text = "Año";
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Location = new System.Drawing.Point(354, 216);
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(100, 23);
+            this.txtAnio.TabIndex = 9;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(273, 256);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(40, 15);
+            this.lblPrecio.TabIndex = 10;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(354, 253);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 23);
+            this.txtPrecio.TabIndex = 11;
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(273, 290);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(36, 15);
+            this.lblStock.TabIndex = 12;
+            this.lblStock.Text = "Stock";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(354, 290);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 23);
+            this.txtStock.TabIndex = 13;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(354, 338);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(75, 23);
+            this.btnGrabar.TabIndex = 14;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
             // 
             // frmVehiculo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 494);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txtAnio);
+            this.Controls.Add(this.lblAnio);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.cboDescripcion);
+            this.Controls.Add(this.lblDescripcion);
             this.Controls.Add(this.txtModelo);
             this.Controls.Add(this.lblModelo);
             this.Controls.Add(this.txtMarca);
@@ -80,6 +193,7 @@
             this.Name = "frmVehiculo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AUTOMOTRIZ APP - VEHICULOS";
+            this.Load += new System.EventHandler(this.frmVehiculo_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,5 +205,16 @@
         private TextBox txtMarca;
         private Label lblModelo;
         private TextBox txtModelo;
+        private Label lblDescripcion;
+        private ComboBox cboDescripcion;
+        private Label lblColor;
+        private TextBox txtColor;
+        private Label lblAnio;
+        private TextBox txtAnio;
+        private Label lblPrecio;
+        private TextBox txtPrecio;
+        private Label lblStock;
+        private TextBox txtStock;
+        private Button btnGrabar;
     }
 }
--- AutomotrizApp/AutomotrizFront/frmVehiculo.cs
+++ AutomotrizApp/AutomotrizFront/frmVehiculo.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutomotrizApp.dominio;
+using AutomotrizFront.Http;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,58 @@ namespace AutomotrizFront
 {
     public partial class frmVehiculo : Form
     {
+        private string[] listaTipos = new string[4];
+        private Producto vehiculo = new();
         public frmVehiculo()
         {
             InitializeComponent();
+            listaTipos[0] = "Coupe";
+            listaTipos[1] = "Urbano";
+            listaTipos[2] = "Sedan";
+            listaTipos[3] = "Pickup";
+            cboDescripcion.SelectedIndex = -1;
+        }
+
+        private void frmVehiculo_Load(object sender, EventArgs e)
+        {
+            cboDescripcion.DataSource = listaTipos;
+        }
+
+        private async void btnGrabar_Click(object sender, EventArgs e)
+        {
+            if (txtAnio.Text == "" || txtMarca.Text == "" || txtModelo.Text == "" || txtColor.Text == "" || txtAnio.Text == "" || txtPrecio.Text == "" || cboDescripcion.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe rellenar todos los campos para grabar el vehículo!", "Control", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            vehiculo.Id_Producto = DateTime.Now.Hour / DateTime.Now.Hour + 20;
+            vehiculo.Anio = Convert.ToInt32(txtAnio.Text);
+            vehiculo.Marca = txtMarca.Text;
+            vehiculo.Modelo = txtModelo.Text;
+            vehiculo.Descripcion = cboDescripcion.SelectedText;
+            vehiculo.Precio_Vta = Convert.ToDouble(txtPrecio.Text);
+            vehiculo.Color = txtColor.Text;
+            var result = this.GrabarVehiculoAsync();
+        }
+
+        private async Task GrabarVehiculoAsync()
+        {
+
+
+            string pedidoJSON = JsonConvert.SerializeObject(vehiculo);
+
+            string url = "http://localhost:5008/api/Producto/Crear";
+            var result = await ClienteSingleton.ObtenerInstancia().PostAsyncDefault(url, pedidoJSON);
+
+            if (result.Equals("true"))
+            {
+                MessageBox.Show("Vehículo registrado con éxito!", "Informe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("ERROR. No se pudo registrar el vehículo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
